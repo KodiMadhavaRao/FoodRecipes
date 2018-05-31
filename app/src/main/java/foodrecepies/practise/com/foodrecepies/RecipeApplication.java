067bbd15a5c0f12be4ccc771e0ac34c92fe16df9
Droid <mailtosonumadhav@gmail.com>
2018-05-31 10:55:47 +0530

    revert

--- app/src/main/java/foodrecepies/practise/com/foodrecepies/RecipeApplication.java
+++ app/src/main/java/foodrecepies/practise/com/foodrecepies/RecipeApplication.java
@@ -3,7 +3,7 @@ package foodrecepies.practise.com.foodrecepies;
 import android.app.Application;
 
 public class RecipeApplication extends Application {
-    public static RecipeApplication appContext;
+    private RecipeApplication appContext;
 
     @Override
     public void onCreate() {
